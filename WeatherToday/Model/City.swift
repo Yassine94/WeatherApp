23fa48480a56c34e1288e0bc842c009104e50c57
Yassine FATIHI <20181092@B15-16.local>
2019-05-24 09:22:09 +0200

    adding files
    podfile
    controlers
    data model

---
+++ WeatherToday/Model/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  WeatherToday
+//
+//  Created by Yassine FATIHI on 23/05/2019.
+//  Copyright © 2019 Yassine FATIHI. All rights reserved.
+//
+
+
+import CoreLocation
+
+struct City {
+    var name: String
+    var coordinates: CLLocationCoordinate2D
+}
---
+++ WeatherToday/Model/WeatherMesure.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherMesure.swift
+//  WeatherToday
+//
+//  Created by Yassine FATIHI on 23/05/2019.
+//  Copyright © 2019 Yassine FATIHI. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Decodable {
+    var currently: Currently
+    var hourly: Hourly
+    var daily : Daily
+}
+struct Currently : Decodable {
+    var icon: String
+    var temperature: Double
+    var summary: String
+    var pressure: Double
+    var uvIndex: Int
+    var windSpeed: Double
+    var humidity: Double
+}
+struct Hourly:Decodable {
+    var summary: String
+    var data : [Datas]
+}
+struct Datas:Decodable {
+    var time: Int
+    var icon: String
+    var humidity: Double
+    var temperature: Double
+}
+
+struct Daily: Decodable {
+    var summary: String
+    var data: [DataDaily]
+}
+struct  DataDaily: Decodable {
+    var time: Int
+    var icon: String
+    var temperatureMin: Double
+    var temperatureMax: Double
+}
